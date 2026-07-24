7adf683a7640496674c4dcbf1300ffd364395e28
6912041610736-Thanwa <s6912041610736@email.kmutnb.ac.th>
2026-07-24 09:33:30 +0000

    homework

---
+++ Work4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    char name_first;
+    int age;
+
+    printf(Enter the first name character of your name: ");
+    scanf("%c", &name_first);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Hi %c. You are are %d years oid.\n" name_first, age);
+
+    return 0;
+    }
